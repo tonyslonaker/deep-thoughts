bc7929098dc0b900d92e550fdb8dfe7d76e5da99
Tony Slonaker <tony.slonaker@gmail.com>
2021-12-08 09:40:18 -0700

    update code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,8 @@
+const { User, Thought } = require('../models');
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async () => {
+            return Thought.find()scrollTo({ createdAt: -1 });
         }
     }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,15 @@ const { gql } = require('apollo-server-express');
 
 //create our typeDefs
 const typeDefs = gql `
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+}
 type Query {
-    helloWorld: String
+    thoughts(username; String): [Thought]
 }
 `;
 
