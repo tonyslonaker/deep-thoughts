d319b145295c51b6f89834e1c285ddfe3944593f
Tony Slonaker <tony.slonaker@gmail.com>
2021-12-09 10:06:51 -0700

    update queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,9 +9,28 @@ type Thought {
     createdAt: String
     username: String
     reactionCount: Int
+    reactions: [Reaction]
+}
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
 }
 type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username; String): [Thought]
+    thought(_id: ID!): Thought
+
 }
 `;
 
